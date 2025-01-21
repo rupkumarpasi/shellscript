96e34e5dc5777b6a849bad9d69251ae4597ad87b
rupkumar <memoryclass12@gmail.com>
2025-01-21 10:24:31 +0545

    first commit

---
+++ code.sh
@@ -0,0 +1,8 @@
+git init
+git add code.sh
+git config --global user.name "rupkumar"
+git config --global user.email "memoryclass12@gmail.com"
+git commit -m "first commit"
+git branch -m main
+git remote add origin https://github.com/rupkumarpasi/shellscript.git
+git push -u origin main
